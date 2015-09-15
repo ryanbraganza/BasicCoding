7c1f8d013d8c11f6b7dfced60453a79451c4f67d
Dylan Marriott <dmarriott@evernote.com>
2015-09-15 22:36:41 +0200

    More levels

---
+++ Lesson01/levels/level04.js
@@ -0,0 +1,9 @@
+_levelNr = 4;
+delay = 500;
+
+function exec() {
+	
+	// Repetitive tasks can be put in functions. We can call a function at any time. Cool, right?
+	// Because we're already inside a function (to make this all work) we need to create the function outside of this one. 
+	
+}
---
+++ Lesson01/levels/level05.js
@@ -0,0 +1,9 @@
+_levelNr = 5;
+delay = 100;
+
+function exec() {
+	
+	// new function -> getX(); and getY();
+	// use these to check that you don't leave the playing field
+	
+}
